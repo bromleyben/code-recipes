26df8b08bfb9cfcef3ec51c8eb6f2df41eee25a4
Ben Bromley <ben@inspiredsoftware.com.au>
2020-06-23 12:20:41 +1000

    Add website form change recipes

---
+++ Odoo/Javascript/website_form.js
@@ -0,0 +1,43 @@
+odoo.define('module.name', function (require) {
+    "use strict";
+
+    // Extending the website form
+    const WebsiteForm = require('website_form.animation'),  // Require this to ensure correct load order
+          snippet_animation = require('web_editor.snippets.animation');
+
+    // Adding a spinner on submit and removing when errors occur
+    {
+    snippet_animation.registry.form_builder_send.include({
+
+        /**
+         * Extend base function to add a spinner to the website form submit button
+         * @param {Event} e
+         * @returns {*} Super
+         */
+        send(e) {
+            e.preventDefault();
+
+            // Create spinner
+            this.$spinner = $('<i class="fa fa-cog fa-spin" style="margin-left: 8px"/>');
+
+            // Add to send button
+            this.$target.find('.o_website_form_send').append(this.$spinner)
+
+            return this._super.apply(this, arguments);
+        },
+
+        /**
+         * Removes spinner from button if an error occurred
+         * @param {string} status The status of the form submit
+         * @returns {*} Super
+         */
+        update_status(status) {
+            if (status !== 'success' && this.$spinner) {
+                this.$spinner.remove();
+            }
+
+            return this._super.apply(this, arguments);
+        },
+    })
+    }
+});
